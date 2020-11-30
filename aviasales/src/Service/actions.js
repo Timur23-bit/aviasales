4a002911c89b442677bb482f50e8c6643a2cb74c
Timur23-bit <62034798+Timur23-bit@users.noreply.github.com>
2020-11-30 15:21:03 +0400

    art

---
+++ aviasales/src/Service/actions.js
@@ -0,0 +1,8 @@
+export const all = (checked) => ({ type: 'ALL', checked });
+export const noAll = (checked) => ({ type: 'NO_ALL', checked });
+export const noStops = (checked) => ({ type: 'NO_STOPS', checked });
+export const one = (checked) => ({ type: 'ONE', checked });
+export const two = (checked) => ({ type: 'TWO', checked });
+export const three = (checked) => ({ type: 'THREE', checked });
+export const less = () => ({ type: 'LESS' });
+export const quickly = () => ({ type: 'QUICK' });
